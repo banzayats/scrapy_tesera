67a980cb1b8d47d0fa89d63af93e01bda95bf93d
Vadim Kalsin <neformat@gmail.com>
2016-11-16 09:52:17 +0200

    Modified spider: added some fields. Excluded json files from project.

--- tesera/spiders/games_spider.py
+++ tesera/spiders/games_spider.py
@@ -1,10 +1,11 @@
+# -*- coding: utf-8 -*-
 import scrapy
 
 
 class GamesSpider(scrapy.Spider):
     name = "games"
 
-    start_urls = ['http://tesera.ru/games/all/%s/' % page for page in xrange(0,2)]
+    start_urls = ['http://tesera.ru/games/all/%s/' % page for page in xrange(0,10)]
 
     def parse(self, response):
         # follow links to game pages
@@ -15,7 +16,7 @@ class GamesSpider(scrapy.Spider):
 
     def parse_game(self, response):
 	def extract_with_xpath(query):
-	    return response.xpath(query).extract_first().strip()
+	    return response.xpath(query).extract_first()
 
         yield {
 	    'title_main': extract_with_xpath('//*[@id="game_title"]/span/text()'),
@@ -24,6 +25,11 @@ class GamesSpider(scrapy.Spider):
             'time_learn':  extract_with_xpath('//ul[@class="classnav"]/li[4]/text()'),
             'time_game':  extract_with_xpath('//ul[@class="classnav"]/li[5]/text()'),
 	    'rating_summ': extract_with_xpath('//div[@class="colright"]/div/h3/span/span/text()'),
+	    'num_comments': extract_with_xpath('//div[@class="block comment"]//div[@class="numb"]/text()'),
+            'num_news': extract_with_xpath('//div[@class="block news"]//div[@class="numb"]/text()'),
+            'num_similar': extract_with_xpath('//div[@class="block similar"]//div[@class="numb"]/text()'),
+	    'num_users_own': extract_with_xpath('//div[@class="smalltab-green smt"]//a[contains(text(), "владеют")]/../following-sibling::span/text()'),
+
 	}
 
 
